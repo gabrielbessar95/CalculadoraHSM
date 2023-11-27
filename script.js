a765b25a19ce7687247df1219b6a247e0601ef6c
gabrielbessar95 <80074356+gabrielbessar95@users.noreply.github.com>
2023-11-27 12:54:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function insert(num) {
+    document.querySelector(".tela").innerHTML += num;
+  }
+  
+  function clean() {
+    document.querySelector(".tela").innerHTML = "";
+  }
+  
+  function back() {
+    let tela = document.querySelector(".tela").innerHTML;
+    document.querySelector(".tela").innerHTML = tela.substring(
+      0,
+      tela.length - 1
+    );
+  }
+  
+  function calcular() {
+    let tela = document.querySelector(".tela").innerHTML;
+    if (tela) {
+      document.querySelector(".tela").innerHTML = eval(tela);
+    } else {
+      document.querySelector(".tela").innerHTML = "Digite a operação";
+    }
+  }
